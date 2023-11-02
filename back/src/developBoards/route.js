a3902e730d7dee401ca8b1085c9caedd83a05be5
sangheun969 <sang969@naver.com>
2023-11-02 18:21:56 +0900

    feat: new feature

--- back/src/developBoards/route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bodyParser = require("body-parser");
-
-router.use(bodyParser.urlencoded({ extended: false }));
-// router.get
-
-module.exports = router;
