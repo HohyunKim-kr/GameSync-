e5f224656d57622d7fa541f1a8602633bb370cf5
sangheun969 <sang969@naver.com>
2023-11-02 19:12:07 +0900

    feat: dev.route.js

---
+++ back/src/developBoards/board/dev.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+// const devController = require("./dev.controller");
+
+// list
+// router.get("/", devController.findAll);
+
+// write
+// router.post("/", devController.create);
+
+// view
+// router.get("/:id", devController.findOne);
+
+// modify
+// router.put("/:id", devController.update);
+
+// delete
+// router.delete("/:id", devController.delete);
+
+module.exports = router;
