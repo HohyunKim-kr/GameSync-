f7a90e020f0a7d5c92298af6ce66207ab9e79bd2
Hohyun.Kim <hohyun1022@naver.com>
2023-11-03 13:48:01 +0900

    fix: err module.js

--- back/src/ideaBoards/ideaBoard.module.js
+++ back/src/ideaBoards/ideaBoard.module.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 const IdeaService = require("./idea.service");
 const IdeaController = require("./idea.controller");
-=======
 const IdeaService = require("./ideaBoard.service");
 const IdeaController = require("./ideaBoard.controller");
->>>>>>> 2b8c0f218015e470886155af444e4a35feef3234
 const { IdeaBoards } = require("../entity");
 
 const ideaService = new IdeaService(IdeaBoards);
