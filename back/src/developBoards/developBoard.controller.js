0818b950d514e7b5288abba387100c49bc93ce81
sangheun969 <sang969@naver.com>
2023-11-03 14:33:54 +0900

    fix: developBoard.rout,controller

--- back/src/developBoards/developBoard.controller.js
+++ back/src/developBoards/developBoard.controller.js
@@ -1,20 +1,20 @@
-class DevelopController {
-  constructor(service) {
-    this.service = service;
-  }
+// class DevelopController {
+//   constructor(service) {
+//     this.service = service;
+//   }
 
-  async create(req, res, next) {
-    try {
-    } catch (e) {
-      next(e);
-    }
-  }
-}
+//   async create(req, res, next) {
+//     try {
+//     } catch (e) {
+//       next(e);
+//     }
+//   }
+// }
 
-module.exports = DevelopController;
+// module.exports = DevelopController;
 
-// exports.create = async (req, res, next) => {};
-// exports.findAll = async (req, res, next) => {};
-// exports.findOne = async (req, res, next) => {};
-// exports.update = async (req, res, next) => {};
-// exports.delete = async (req, res, next) => {};
+exports.create = async (req, res, next) => {};
+exports.findAll = async (req, res, next) => {};
+exports.findOne = async (req, res, next) => {};
+exports.update = async (req, res, next) => {};
+exports.delete = async (req, res, next) => {};
