c12abfa61d4f0368a910c9bb3602f8bbc75420eb
sangheun969 <sang969@naver.com>
2023-11-03 10:19:46 +0900

    fix: try pull

--- back/src/developBoards/board/devBoards.controller.js
+++ back/src/developBoards/board/devBoards.controller.js
@@ -1,20 +1,20 @@
-class DevController {
-  constructor(service) {
-    this.service = service;
-  }
+// class DevController {
+//   constructor(service) {
+//     this.service = service;
+//   }
 
-  async create(req, res, next) {
-    try {
-    } catch (e) {
-      next(e);
-    }
-  }
-}
+//   async create(req, res, next) {
+//     try {
+//     } catch (e) {
+//       next(e);
+//     }
+//   }
+// }
 
-module.exports = DevController;
+// module.exports = DevController;
 
-// exports.create = async (req, res, next) => {};
-// exports.findAll = async (req, res, next) => {};
-// exports.findOne = async (req, res, next) => {};
-// exports.update = async (req, res, next) => {};
-// exports.delete = async (req, res, next) => {};
+exports.create = async (req, res, next) => {};
+exports.findAll = async (req, res, next) => {};
+exports.findOne = async (req, res, next) => {};
+exports.update = async (req, res, next) => {};
+exports.delete = async (req, res, next) => {};
