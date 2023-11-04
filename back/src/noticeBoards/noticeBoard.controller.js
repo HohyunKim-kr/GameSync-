e41adfe4890fe5b33717fb4b4e29116d04cb820c
Hohyun.Kim <hohyun1022@naver.com>
2023-11-04 22:48:13 +0900

    feat: noticeBoard Create tdd

--- back/src/noticeBoards/noticeBoard.controller.js
+++ back/src/noticeBoards/noticeBoard.controller.js
@@ -1,19 +1,17 @@
-// class IdeaController {
-//     constructor(service) {
-//         this.service = service;
-//     }
+const { noticecBoardRequesDTO } = require("./noticeBoard.dto");
+const noticeBoardService = require("./noticeBoard.service");
 
-//     async create(req, res, next) {
-//         try {
-//         } catch (e) {
-//             next(e);
-//         }
-//     }
-// }
-
-// module.exports = IdeaController;
-
-exports.create = async (req, res, next) => {};
+exports.create = async (req, res, next) => {
+  try {
+    const noticecBoardRequesDTO = new noticecBoardRequesDTO(req.body);
+    const response = await noticeBoardService.createBoard(
+      noticecBoardRequesDTO
+    );
+    res.status(201).json(response);
+  } catch (e) {
+    next(e);
+  }
+};
 exports.findAll = async (req, res, next) => {};
 exports.findOne = async (req, res, next) => {};
 exports.update = async (req, res, next) => {};
--- back/src/noticeBoards/noticeBoard.module.js
+++ back/src/noticeBoards/noticeBoard.module.js
@@ -1,6 +1,5 @@
 const noticeService = require("./noticeBoard.service");
 const noticeController = require("./noticeBoard.controller");
-const noticeService = require("./noticeBoard.service");
 const { noticeBoards } = require("../entity");
 
 const noticeService = new noticeService(noticeBoards);
