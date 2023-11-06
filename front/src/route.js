b281f8518ca8d583a8c1ed453d2d454e286a8ff5
boram-blockchain <penguniee@gmail.com>
2023-11-06 09:14:01 +0900

    feat : ideaBoard front 설정

---
+++ front/src/route.js
@@ -0,0 +1,17 @@
+// const app = require("../app");
+
+// app.get("/ideaBoards/", (req, res) => {
+//     res.render("ideaBoards/ideaBoard.list.html");
+// });
+
+// app.get("/ideaBoards/write", (req, res) => {
+//     res.render("ideaBoards/ideaBoard.write.html");
+// });
+
+// app.get("/ideaBoards/view", (req, res) => {
+//     res.render("ideaBoards/ideaBoard.view.html");
+// });
+
+// app.get("/ideaBoards/modify", (req, res) => {
+//     res.render("ideaBoards/ideaBoard.modify.html");
+// });
