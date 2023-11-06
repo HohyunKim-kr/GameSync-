7766ca4cda8dc19e1869df1fc5d48cc4b61df09d
Hohyun.Kim <hohyun1022@naver.com>
2023-11-06 13:32:01 +0900

    fix: route table

--- front/src/route.js
+++ front/src/route.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const router = express.Router();
 // const developBoardsRouter = require("./developBoards/developBoard.route");
-const ideaBoardsRouter = require("./ideaBoards/ideaBoard.route");
-// const noticesRouter = require("./notices/noticeBoard.route");
+// const ideaBoardsRouter = require("./ideaBoards/ideaBoard.route");
+const noticesRouter = require("./notices/noticeBoard.route");
 
-router.get("/", (req, res) => {
-  res.render("index.html");
-});
+// router.get("/", (req, res) => {
+//   res.render("index.html");
+// });
 
 // router.use("/develop", developBoardsRouter);
-router.use("/idea", ideaBoardsRouter);
-// router.use("/notice", noticesRouter);
+// router.use("/idea", ideaBoardsRouter);
+router.use("/notice", noticesRouter);
 
 module.exports = router;
