1ab97e08b03065906c9b1e82f3ce883f7e284601
Hohyun.Kim <hohyun1022@naver.com>
2023-11-07 13:28:52 +0900

    fiex

--- front/app.js
+++ front/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const nunjucks = require("nunjucks");
 const router = require("./src/route");
-const multer = require("multer");
 
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -13,20 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.static("uploads"));
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: (req, file, done) => {
-      done(null, "./uploads");
-    },
-    filename: (req, file, done) => {
-      const ext = path.extname(file.originalname); // 확장자
-      // aws1, .png
-      const filename =
-        path.basename(file.originalname, ext) + "_" + Date.now() + ext;
-      done(null, filename);
-    },
-  }),
-});
 
 app.use(router);
 
