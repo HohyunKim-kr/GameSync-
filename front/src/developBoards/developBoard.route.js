76e650c297176c84f90e47c1763f7a91a55c5fb6
sangheun969 <sang969@naver.com>
2023-11-07 10:14:50 +0900

    fix:All developBoards

--- front/src/developBoards/developBoard.route.js
+++ front/src/developBoards/developBoard.route.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-const developBoardController = require("./developBoards.controller");
+// const developBoardController = require("./developBoards.controller");
 
-router.get("/developBoards/", developBoardController.list);
-router.post("/developBoards/write", developBoardController.write);
-router.get("/developBoards/view", developBoardController.view);
-router.post("/developBoards/modify", developBoardController.modify);
+// router.get("/developBoards/", developBoardController.list);
+// router.post("/developBoards/write", developBoardController.write);
+// router.get("/developBoards/view", developBoardController.view);
+// router.post("/developBoards/modify", developBoardController.modify);
 
-module.exports = router;
+// module.exports = router;
