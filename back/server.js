9d9c8ed05923b6af58771a280f509b87a6714b49
sangheun969 <sang969@naver.com>
2023-11-09 09:41:56 +0900

    fix: gitignore

--- back/server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const { sequelize } = require("./src/entity");
-
-app.listen(4000, async () => {
-  await sequelize.sync({ force: true });
-  console.log(`DB Connection`);
-  console.log("backend server start");
-});
