1896f32630e4dea99e52a332fd79f3825108bad6
Hohyun.Kim <hohyun1022@naver.com>
2023-11-09 11:40:14 +0900

    fix: user Route

--- front/src/route.js
+++ front/src/route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-// const usersRouter = require("./users/user.route");
+const usersRouter = require("./users/user.route");
 const developBoardsRouter = require("./developBoards/developBoard.route");
 const ideaBoardsRouter = require("./ideaBoards/ideaBoard.route");
 const noticesRouter = require("./notices/noticeBoard.route");
@@ -9,7 +9,7 @@ router.get("/", (req, res) => {
   res.render("index.html");
 });
 
-// router.use("/users", usersRouter);
+router.use("/users", usersRouter);
 router.use("/develop", developBoardsRouter);
 router.use("/idea", ideaBoardsRouter);
 router.use("/notice", noticesRouter);
