109031d3dbb47e2c21b3a34773dc8508701477ad
Hohyun.Kim <hohyun1022@naver.com>
2023-11-09 10:36:50 +0900

    feat: userRoute

---
+++ front/src/users/user.route.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("./user.controller");
+
+//login
+router.get("/login", userController.getLogin);
+router.post("/login", userController.postLogin);
+router.get("/logout", userController.getLogout);
+
+// sign-up
+router.get("/signup", userController.getSignup);
+router.post("/signup", userController.postSignup);
+
+// usereInfo
+router.get("/user", userController.getUser);
+router.get("/user/modify", userController.getUsermodify);
+router.post("/user/modify", userController.postUsermodify);
+router.get("/user/delete", userController.getUserdelete);
+
+module.exports = router;
