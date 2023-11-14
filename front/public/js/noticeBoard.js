d479952142ac8225d68c16f33eafa0bf7c72717b
boram-blockchain <penguniee@gmail.com>
2023-11-14 19:44:55 +0900

    fix : style 수정

--- front/public/js/noticeBoard.js
+++ front/public/js/noticeBoard.js
@@ -27,20 +27,3 @@
 //   const { data } = await axios.post("http://localhost:4000/noticeBoards/", tmp);
 //   console.log(data);
 // });
-
-function previewImage(input) {
-    let preview = document.getElementById("thumbnailPreview");
-    let file = input.files[0];
-    let reader = new FileReader();
-
-    reader.onloadend = function () {
-        preview.innerHTML =
-            '<img src="' + reader.result + '" alt="썸네일 미리보기" />';
-    };
-
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
-        preview.innerHTML = "";
-    }
-}
