4dddfe0e6b27a020de348fceb6d92548f3a4923b
sangheun969 <sang969@naver.com>
2023-11-14 09:19:44 +0900

    fix: login Cookies

--- front/src/users/user.service.js
+++ front/src/users/user.service.js
@@ -28,20 +28,13 @@ async function postLogin(user_email, user_pw) {
 // 회원 가입 요청을 보내는 함수
 async function postSignup(userData) {
   try {
-    const response = await fetch(`${API_URL}/users/signup`, {
-      method: "POST",
+    const response = await axios.post(`${API_URL}/users/signup`, userData, {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(userData),
     });
 
-    if (!response.ok) {
-      throw new Error("회원 가입에 실패했습니다.");
-    }
-
-    const data = await response.json();
-    return data.token; // 회원 가입에 성공하면 백엔드로부터 토큰을 반환
+    return response.data.token;
   } catch (error) {
     throw error;
   }
