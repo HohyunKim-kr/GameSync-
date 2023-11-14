86c7821c9ff8f439d6f46254387733c082b20c42
boram-blockchain <penguniee@gmail.com>
2023-11-14 13:29:55 +0900

    feat : kakao login 구현

--- front/src/users/user.controller.js
+++ front/src/users/user.controller.js
@@ -1,50 +1,85 @@
-const { postLogin, postSignup } = require("./user.service");
+const { postLogin, postSignup, kakaoLogin } = require("./user.service");
+require("dotenv").config();
+
+const REST_API_KEY = process.env.REST_API_KEY;
+const KAKAO_REDIRECT_URI = process.env.KAKAO_REDIRECT_URI;
 
 exports.getLogin = (req, res) => {
-  res.render("users/login.html");
+    res.render("users/login.html");
 };
 
 exports.getSignup = (req, res) => {
-  res.render("users/signup.html");
+    res.render("users/signup.html");
 };
 
 exports.getUser = (req, res) => {
-  res.render("users/mypage.html");
+    res.render("users/mypage.html");
 };
 
 exports.getAdmin = (req, res) => {
-  res.render("admin/adminpage.html");
+    res.render("admin/adminpage.html");
 };
 
 exports.getUsermodify = (req, res) => {
-  res.render("users/mypage.modify.html");
+    res.render("users/mypage.modify.html");
 };
 
 exports.postLogin = async (req, res, next) => {
-  try {
-    const { user_email, user_pw } = req.body;
-    const token = await postLogin(user_email, user_pw);
+    try {
+        const { user_email, user_pw } = req.body;
+        const token = await postLogin(user_email, user_pw);
 
-    if (token) {
-      res.cookie("cookie", token);
+        if (token) {
+            res.cookie("cookie", token);
 
-      return res.redirect("/");
+            return res.redirect("/");
 
-      // return res.status(200).json({ token });
-    } else {
-      return res.status(401).json({ message: "Post Login failed" });
+            // return res.status(200).json({ token });
+        } else {
+            return res.status(401).json({ message: "Post Login failed" });
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 exports.postSignup = async (req, res, next) => {
-  try {
-    const userData = req.body;
-    const token = await postSignup(userData);
-    return res.status(201).json({ token });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const userData = req.body;
+        const token = await postSignup(userData);
+        return res.status(201).json({ token });
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.kakaoLogin = (req, res, next) => {
+    try {
+        const redirectURI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+        res.redirect(redirectURI);
+    } catch (e) {
+        next(e);
+    }
+};
+
+exports.kakaoCallback = async (req, res, next) => {
+    try {
+        const { code } = req.query;
+        // console.log(`code:`, code);
+        const result = await kakaoLogin(code);
+        // console.log(`넘어와!!!:`, result.data.user);
+        // console.log(`넘어와!!!:`, result.data.token);
+        const { token } = result.data;
+        if (token) {
+            res.cookie("cookie", token);
+
+            return res.redirect("/");
+
+            // return res.status(200).json({ token });
+        } else {
+            return res.status(401).json({ message: "kakao Login failed" });
+        }
+    } catch (e) {
+        next(e);
+    }
 };
